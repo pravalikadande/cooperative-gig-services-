95847495498f0e87458ce78cec2a79fe4076514a
pravalikadande <dandeprvalika160@gmail.com>
2026-09-04 11:35:35 +0530

    Update json files

--- app.config.ts
+++ app.config.ts
@@ -59,6 +59,8 @@ const config: ExpoConfig = {
       }
   },
   android: {
+    googleServicesFile: "./google-services.json",
+
     adaptiveIcon: {
       backgroundColor: "#E6F4FE",
       foregroundImage: "./assets/images/android-icon-foreground.png",
